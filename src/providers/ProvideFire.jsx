d32dca9be9f1f897341cfecc8d07579f3f49981e
timothytjoe <ttjoe0@gmail.com>
2023-09-21 17:28:55 +0000

    Install dependencies

---
+++ src/providers/ProvideFire.jsx
@@ -0,0 +1,44 @@
+import { useEffect, useState } from 'react';
+
+const useLiberiaFireData = () => {
+    const [fireData, setFireData] = useState(null);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                // Define the URLs or endpoints for the three data sources
+                const NASA_API_LIB = import.meta.env.VITE_NASA_FIRMS_API_URL_LIB;
+                const NASA_API_WORLD = import.meta.env.VITE_NASA_FIRMS_API_URL_WORLD;
+                const source2 = 'https://source2-api.com/liberia-fire-data';
+                const source3 = 'https://source3-api.com/liberia-fire-data';
+
+                // Fetch data from all three sources concurrently using Promise.all()
+                const [data1, data2, data3] = await Promise.all([
+                    fetch(NASA_API_LIB).then((response) => response.json()),
+                    fetch(source2).then((response) => response.json()),
+                    fetch(source3).then((response) => response.json()),
+                ]);
+
+                // Process and combine the data as needed
+                const combinedData = {
+                    NASAData: data1,
+                    source2Data: data2,
+                    source3Data: data3,
+                };
+
+                // Set the combined data in the state
+                setFireData(combinedData);
+            } catch (error) {
+                console.error('Error fetching fire data:', error);
+                setFireData(null);
+            }
+        };
+
+        // Call the fetchData function when the component mounts
+        fetchData();
+    }, []); // Empty dependency array to run once when mounted
+
+    return fireData;
+};
+
+export default useLiberiaFireData;
