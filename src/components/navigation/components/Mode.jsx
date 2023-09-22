b2b3cc116e8770b75bdc75d75f994130f4ee56a2
timothytjoe <ttjoe0@gmail.com>
2023-09-22 06:36:56 +0000

    Updated Mode Icons to change between state

--- src/components/navigation/components/Mode.jsx
+++ src/components/navigation/components/Mode.jsx
@@ -1,11 +1,19 @@
-import { RxSun } from "react-icons/rx"
+import { RxSun, RxMoon } from "react-icons/rx"
 import { Button, useTheme } from "@mui/material"
 import { useMode } from "@providers/ProvideMode"
 
 function Mode() {
     const theme = useTheme()
     const { switchMode } = useMode()
-    return (<Button onClick={switchMode} startIcon={<RxSun />}>{theme.palette.mode}</Button>)
+    const icon = theme.palette.mode === "light" ? <RxSun /> : <RxMoon />
+
+
+    return (
+        <Button
+            onClick={switchMode}
+            startIcon={icon}>
+            {theme.palette.mode}
+        </Button>)
 }
 
 export default Mode
