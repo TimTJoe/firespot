b2c4369aa7372e44c3f5ce79a1817773047bcfd5
timothytjoe <ttjoe0@gmail.com>
2023-09-22 06:44:32 +0000

    Removed unused dependencies

---
+++ src/components/Notice.jsx
@@ -0,0 +1,11 @@
+import styled from "@emotion/styled"
+import { Popper } from "@mui/material"
+
+const Container = styled(Popper)`
+    
+`
+export default function Notice() {
+    return (
+        <Container>Notice</Container>
+    )
+}
